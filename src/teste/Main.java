114dee49ab10d79c6f0e33d966b1abafd7f4fd3a
Rachel <rachellizandra@outlook.com>
2022-12-19 22:50:13 -0300

    adicionamos resolução da primeira atividade

--- src/teste/Main.java
+++ src/teste/Main.java
@@ -43,8 +43,35 @@ public class Main {
                     if(cliente1.conta.saldo > 0) {
                         System.out.println("Digite o valor que você quer sacar:");
                         valor = scan.nextDouble();
-                        if()
+                        if(valor <= cliente1.conta.saldo && valor > 0) {
+                            cliente1.conta.saldo -= valor;
+                            System.out.println("Seu saldo agora é: " + cliente1.conta.saldo);
+                        } else {
+                            System.out.println("Valor inválido! Tente novamente");
+                            break;
+                        }
+
+                    } else {
+                        System.out.println("Você não possui saldo suficiente");
                     }
+                    break;
+                case 3:
+                    System.out.println("Digite o valor que você quer depositar:");
+                    valor = scan.nextDouble();
+                    if(valor > 0) {
+                        cliente1.conta.saldo += valor;
+                        System.out.println("Seu saldo agora é: " + cliente1.conta.saldo);
+                    } else {
+                        System.out.println("Valor inválido! Tente novamente");
+                        break;
+                    }
+                    break;
+                case 0:
+                    clienteAtivo = false;
+                    System.out.println("Você saiu da conta.");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
             }
         }
     }
